109d1e94985fa335bc9ba4ced4855b0ef01ec4da
Andrew Nesbitt <andrewnez@gmail.com>
2008-05-25 21:43:16 +0100

    improved and refactored views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.rss '/feed.rss', :controller => 'index', :action => 'feed', :format => 'atom' # legacy routes
-  map.atom '/feed.atom', :controller => 'index', :action => 'feed', :format => 'atom' # legacy routes
+  map.rss '/feed.rss', :controller => 'index', :action => 'index', :format => 'atom' # legacy routes
+  map.atom '/feed.atom', :controller => 'index', :action => 'index', :format => 'atom' # legacy routes
 
   map.topic '/topics/:topic.atom', :controller => 'index', :action => 'topics', :format => 'atom'
 
