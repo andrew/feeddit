988bcf26f446aae3de264f999ed4d516d018cb4c
Andrew Nesbitt <andrewnez@gmail.com>
2009-02-15 10:52:13 +0000

    Upgraded deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,52 @@
-# TODO Get off deprec upgrade to work with capistrano 2.3.0 and github
-require 'deprec/recipes' 
+# set :domain, "208.75.86.40"
+# role :web, domain
+# role :app, domain
+# role :db,  domain, :primary => true
+# role :scm, domain
+# 
+# set :application, "diggfeedr"
+# set :deploy_to, "/var/www/apps/#{application}"
+# 
+# set :user, "andrew"
+# set :repository, "git://github.com/andrew/feeddit.git"
+# set :rails_env, "production"
+# 
+# set :apache_server_name, domain
+# set :apache_proxy_port, 6000
+# set :apache_proxy_servers, 4
+# 
+# set :mongrel_address, apache_proxy_address
 
-set :domain, "feeddit.com"
-role :web, domain
-role :app, domain
-role :db,  domain, :primary => true
-role :scm, domain
 
-set :application, "diggfeedr"
-set :deploy_to, "/var/www/apps/#{application}"
-
+default_run_options[:pty] = true
+set :scm_verbose, true
+ 
+set :branch, "master"
+set :deploy_via, :remote_cache
+ 
+set :application, "feeddit"
+set :repository,  "git@github.com:andrew/feeddit.git"
+ 
+set :deploy_to, "/var/www/#{application}"
 set :user, "andrew"
-set :repository, "git://github.com/andrew/feeddit.git"
-set :rails_env, "production"
+ 
+set :scm, :git
 
-set :apache_server_name, domain
-set :apache_proxy_port, 6000
-set :apache_proxy_servers, 4
-
-set :mongrel_address, apache_proxy_address
+set :domain, "208.75.86.40"
+ 
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+ 
+set :use_sudo, false
+ 
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+ 
+namespace :deploy do
+  %w(start restart stop).each { |name| task name, :roles => :app do passenger.restart end }
+end
