5eacffaf0954255cbe6f1262cf36b3cf98fad7cc
Andrew Nesbitt <andrewnez@gmail.com>
2010-04-05 13:36:44 +0100

    Removed new relic rpm, using built-in heroku version instead

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,6 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem 'newrelic_rpm'
   config.gem 'haml'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
