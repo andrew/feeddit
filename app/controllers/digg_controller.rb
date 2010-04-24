5b46f9c3ba8309c7885777805a7c72a24cd9c494
Andrew Nesbitt <andrewnez@gmail.com>
2010-04-25 00:19:30 +0100

    Added cache control headers for reverse proxy caching

--- app/controllers/digg_controller.rb
+++ app/controllers/digg_controller.rb
@@ -1,27 +1,29 @@
 class DiggController < ApplicationController
 
   def index
+    expires_in 15.minutes, :public => true
     respond_to do |format|
       format.html { @topics = Digg.new.topics }
       format.atom { @stories = Digg.new.stories("stories/#{upcoming_or_popular}", :count => 100) }
     end
   end
-  
+
   def topic
+    expires_in 15.minutes, :public => true
     respond_to do |format|
       format.atom { @stories = Digg.new.stories("stories/topic/#{params[:topic]}/#{upcoming_or_popular}", :count => 100) }
     end
   end
-    
+
   private
-  
+
   helper_method :upcoming?
   def upcoming?
     params[:upcoming]
   end
-  
+
   def upcoming_or_popular
     upcoming? ? 'upcoming' : 'popular'
   end
-  
+
 end
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,15 +1,17 @@
 class IndexController < ApplicationController
 
   caches_page :index, :opensource, :topics
-  
+
   def index
+    expires_in 1.day, :public => true
     respond_to do |format|
-      format.html 
+      format.html
       format.atom { @stories = Digg.new.stories('stories/popular', :count => 100) }
     end
   end
 
-  def topics 
+  def topics
+    expires_in 3.hours, :public => true
     respond_to do |format|
       format.html { @topics = Digg.new.topics }
       format.atom { @stories = Digg.new.stories("stories/topic/#{params[:topic]}/popular", :count => 100) }
